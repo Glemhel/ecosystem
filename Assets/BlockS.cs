857741b775db10b427c26a711c478708cbcaec09
Glemhel <ya.rudakov2001@yandex.ru>
2018-03-26 23:41:28 +0500

    Version 0

---
+++ Assets/BlockS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockS : MonoBehaviour {
+    [SerializeField]
+    int durability;//прочность
+    string type;//тип
+    Vector3 coordinates;//координаты
+    // Use this for initialization
+    //Проверка, нужно ли уничтожать блок
+    bool Ifdestroy()
+    {
+        return (durability <= 0);
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SoilS.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoilS : BlockS {
+    [SerializeField]
+    int clayness;//глинистость
+    string state;//состояние почвы(тип)
+    int nutriens;//удобрения
+    byte herbaceousness;//травянистость
+    int humidity;//увлажнённость
+    void Change_type() {
+        if (clayness >= 15 && nutriens >= 15 && humidity >= 50)
+        {
+            //глина
+            this.state = "глина";
+        }
+        else
+            if (clayness >= 10 && nutriens >= 10 && humidity >= 60)
+        {
+            //торф
+            this.state = "торф";
+        }
+        else
+            if (clayness >= 5 && nutriens >= 5 && humidity >= 30)
+        {
+            //илистые почвы
+            this.state = "ил";
+        }
+        else
+        {
+            //песок
+            this.state = "песок";
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
